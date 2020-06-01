abdc0c10782c3ad40a0b7fe7decbcc6253b541be
Artem Supryhan <suprigan.artem1@gmail.com>
2020-06-02 00:45:18 +0300

    add human target

---
+++ Target/HumanTarget.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_work.Target
+{
+    class HumanTarget : ITarget
+    {
+        public string[] GetTarget()
+        {
+            string[] target = new string[]
+            {
+            @"1            .#####.       ",
+            @"2            |_____|       ",
+            @"3           (\#/ \#/)      ",
+            @"4            |  U  |       ",
+            @"5            \  _  /       ",
+            @"6             \___/        ",
+            @"7         .---'   `---.    ",
+            @"8        /  #########  \   ",
+            @"9       /  |####|####|  \  ",
+            @"10     /  /\ ####### /\  \ ",
+            @"11    (  \  \  ###  /  /  )",
+            @"12     \  \  \_###_/  /  / ",
+            @"13      \  \ |\   /| /  /  ",
+            @"14       'uuu| \_/ |uuu'   ",
+            @"15           |  |  |       ",
+            @"16           |  |  |       ",
+            @"17           |  |  |       ",
+            @"18           |  |  |       ",
+            @"19           |  |  |       ",
+            @"20           )  | (        ",
+            @"21         .oooO Oooo.     "
+            };
+            return target;
+        }
+    }
+}
