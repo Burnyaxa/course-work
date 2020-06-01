638c5016b0f24c6f21e4d20735568d6ec2d1f513
Artem Supryhan <suprigan.artem1@gmail.com>
2020-06-01 23:37:06 +0300

    add base files + circle target

---
+++ Shooter/Shooter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_work.Shooter
+{
+    class Shooter
+    {
+        private static Shooter instance;
+        public string Name { get; private set; }
+        protected Shooter(string name)
+        {
+            Name = name;
+        }
+
+        public static Shooter getInstance(string name)
+        {
+            if (instance == null)
+            {
+                instance = new Shooter(name);
+            }
+            return instance;
+        }
+
+    }
+}
