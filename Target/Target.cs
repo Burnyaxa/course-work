14569f088c2c9a31192d6dab215bd39f2ce4c246
Artem Supryhan <suprigan.artem1@gmail.com>
2020-06-02 19:13:29 +0300

    implement strategy for Target

---
+++ Target/Target.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_work.Target
+{
+    class Target
+    {
+        public string[] TargetMatrix{ get; private set; }
+        public ITarget ITarget { private get; set; }
+
+        public Target(ITarget target)
+        {
+            ITarget = target;
+        }
+
+        public void GetTarget()
+        {
+            TargetMatrix = ITarget.GetTarget();
+        }
+
+    }
+}
