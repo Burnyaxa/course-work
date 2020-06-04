c294a41e687185e0d090a81eac768a3acef37f07
Artem Supryhan <suprigan.artem1@gmail.com>
2020-06-04 10:36:21 +0300

    remove hard code

--- Target/CircleTarget.cs
+++ Target/CircleTarget.cs
@@ -3,37 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace course_work.Target
 {
     class CircleTarget : ITarget
     {
         public string[] GetTarget()
         {
-            string[] target = new string[]
-            {
-                 @"                    *** ### ### ***                  ",
-                 @"                *#                   #*              ",
-                 @"            *#                           #*          ",
-                 @"         *#                                 #*       ",
-                 @"       *#                                     #*     ",
-                 @"     *#                                         #*   ",
-                 @"    *#                                           #*  ", 
-                 @"   *#                                             #* ",
-                 @"  *#                                               #*",
-                 @"  *#                                               #*",
-                 @"  *#                       .                       #*",
-                 @"  *#                                               #*",
-                 @"  *#                                               #*",
-                 @"   *#                                             #* ",
-                 @"    *#                                           #*  ",
-                 @"     *#                                         #*   ",
-                 @"       *#                                     #*     ",
-                 @"         *#                                 #*       ",     
-                 @"            *#                           #*          ",
-                 @"                *#                   #*              ",
-                 @"                    *** ### ### ***                  "
-            };
+            string[] target = ConfigurationManager.AppSettings["circleTarget"].Split(new char[] {','});
+            //string[] target = new string[]
+            //{
+            //     @"                    *** ### ### ***                  ",
+            //     @"                *#                   #*              ",
+            //     @"            *#                           #*          ",
+            //     @"         *#                                 #*       ",
+            //     @"       *#                                     #*     ",
+            //     @"     *#                                         #*   ",
+            //     @"    *#                                           #*  ",
+            //     @"   *#                                             #* ",
+            //     @"  *#                                               #*",
+            //     @"  *#                                               #*",
+            //     @"  *#                       .                       #*",
+            //     @"  *#                                               #*",
+            //     @"  *#                                               #*",
+            //     @"   *#                                             #* ",
+            //     @"    *#                                           #*  ",
+            //     @"     *#                                         #*   ",
+            //     @"       *#                                     #*     ",
+            //     @"         *#                                 #*       ",
+            //     @"            *#                           #*          ",
+            //     @"                *#                   #*              ",
+            //     @"                    *** ### ### ***                  "
+            //};
             return target;
             
         }
--- Target/HumanTarget.cs
+++ Target/HumanTarget.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace course_work.Target
 {
     class HumanTarget : ITarget
     {
         public string[] GetTarget()
         {
-            string[] target = new string[]
-            {
-            @"             .#####.             ",
-            @"             |_____|             ",
-            @"            (\#/ \#/)            ",
-            @"             |  U  |             ",
-            @"             |  _  |             ",
-            @"             |_____|             ",
-            @"           ┌--'---'--┐           ",
-            @"           |         |           ",
-            @"           |    .    |           ",
-            @"           |         |           ",
-            @"           |         |           ",
-            @"           |_________|           "
-            };
+            string[] target = ConfigurationManager.AppSettings["humanTarget"].Split(new char[] { ',' });
+            //string[] target = new string[]
+            //{
+            //@"             .#####.             ",
+            //@"             |_____|             ",
+            //@"            (\#/ \#/)            ",
+            //@"             |  U  |             ",
+            //@"             |  _  |             ",
+            //@"             |_____|             ",
+            //@"           ┌--'---'--┐           ",
+            //@"           |         |           ",
+            //@"           |    .    |           ",
+            //@"           |         |           ",
+            //@"           |         |           ",
+            //@"           |_________|           "
+            //};
             return target;
         }
     }
